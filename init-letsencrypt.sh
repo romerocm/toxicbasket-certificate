9cf2b213e412e8712407f3de665aea082c5cc912
Carlos Romero <cmromero.dev@gmail.com>
2024-11-07 22:36:14 -0600

    fix: Update init-letsencrypt script and docker-compose configuration

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 domains=(heal.toxicbasket.com)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="/etc/letsencrypt"
 email="admin@toxicbasket.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
@@ -10,6 +10,9 @@ staging=0 # Set to 1 if you're testing your setup to avoid hitting request limit
 HOST_IP=$(ip route show | awk '/default/ {print $3}')
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "$HOST_IP heal.toxicbasket.com" >> /etc/hosts
+
+# Create required directories
+mkdir -p "$data_path/live/$domains"
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
